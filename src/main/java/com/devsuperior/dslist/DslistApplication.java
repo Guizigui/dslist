cc6ae209056c843ee369e4818d9ba381f99f5178
Guizigui <gui.diascavalcante@icloud.com>
2025-01-22 20:31:39 -0300

    properties, game , ORM

--- src/main/java/com/devsuperior/dslist/DslistApplication.java
+++ src/main/java/com/devsuperior/dslist/DslistApplication.java
@@ -2,8 +2,10 @@ package com.devsuperior.dslist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.devsuperior")
 public class DslistApplication {
 
 	public static void main(String[] args) {
